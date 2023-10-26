db956c73b085c17e2fc04fc22b88e881ff3c75d1
Combat47 <115223808+combat47@users.noreply.github.com>
2023-10-26 12:05:53 +0330

    Main.java

---
+++ Stage 3 - Miner mania/Main.java
@@ -0,0 +1,37 @@
+package blockchain;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        initBlockchain();
+    }
+
+    public static void initBlockchain() {
+        int processors = Runtime.getRuntime().availableProcessors();
+        BlockChain blockChain = new BlockChain();
+        ExecutorService executorMiner = Executors.newFixedThreadPool(processors);
+
+        for (int i = 0; i < 5; i++) {
+            executorMiner.submit(() -> {
+                try {
+                    long threadId = Thread.currentThread().getId();
+                    blockChain.createBlock(threadId);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        executorMiner.shutdown();//shutdown does not wait for the completion of the executors, so wrap with awaitTermination
+        try {
+            executorMiner.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException interruptedException) {
+            System.out.println("Thread interrupted");
+        }
+    }
+
+}
